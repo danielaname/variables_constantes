342a9a335c681669b014245e95161df2086259b4
codeamac12 <codeamac12@gmail.com>
2017-04-20 18:42:22 -0500

    Variables constantes version 1

---
+++ variables_constantes.rb
@@ -0,0 +1,10 @@
+p title = "programacion"
+p description = "Informacion de programacion"
+p number_of_likes = 10
+p NUMERO_IDENTIFICADOR = 1
+p street = "bosques de eucalitpos"
+p number = "127"
+p state = "mexico"
+p city = "mexico"
+p zip = "11700"
+
